47bac97b7bd34dbc605488d54257ff9473f3b368
Malin Ejdbo <malin.ejdbo@gmail.com>
2019-02-15 14:07:39 +0100

    compared Oneplus6 with Canonn 600D. Fixed so the main file works with help functions.

--- main.m
+++ main.m
@@ -1,12 +1,25 @@
 %% Read in the images and convert to XYZ and LAB
-OnePlus6_RGB = im2double(imread('exebition_1Plus6.tif'));
+clear 
+close all
+clc
+
+original_RGB = im2double(imread('OnePlus6 test2jpg.tif'));
+OnePlus6_RGB = im2double(imread('600D test2jpg.tif'));
+
+%Convert to XYZ and LAB
+original_XYZ = rgb2xyz(original_RGB);
+original_LAB = rgb2lab(original_RGB);
+
 OnePlus6_XYZ = rgb2xyz(OnePlus6_RGB);
 OnePlus6_LAB = rgb2lab(OnePlus6_RGB);
 
+figure;
 imshow(OnePlus6_RGB);
+figure;
+imshow(original_RGB);
 
 %% Calculate SNR, Requires original
-OnePlus6SNR = snr(original_RGB, original_RGB - OnePlus6_RGB)    % 
+OnePlus6SNR = snr(original_RGB, original_RGB - OnePlus6_RGB)    % 17.6207
 
 %% Calculate difference, Requires original
 LAB_diff_1Plus6 = zeros(size(OnePlus6_RGB(:,:,1)));
@@ -17,19 +30,20 @@ for i = 1:size(OnePlus6_RGB(:, 1, 1))
 end
 
 % Calculate mean and max values in difference
-LAB_mean_halftone = mean(mean(LAB_diff_1Plus6)) %
-LAB_max_halftone = max(max(LAB_diff_1Plus6))    %
+LAB_mean_1Plus6 = mean(mean(LAB_diff_1Plus6)) % 0.0056
+LAB_max_1Plus6 = max(max(LAB_diff_1Plus6))    % 28.0498
 
 %% Calculate SNR with filter on, Requires origianl
+% DOES NOT WORK AND DONT KNOW WHY!?
 OnePlus6FilterSNR = snr_filter(original_RGB, original_RGB - OnePlus6_RGB)   %
 
 %% Apply HVS to RGB before calculating color differense, Requires original
-distane = 500; % mm
+distance = 500; % mm
 dpi = 300;
 inch = 25.4; 
 dotSize = 1/dpi * inch; % mm
 
-f = MFTsp(15,dotsize,distance);
+f = MFTsp(15,dotSize,distance);
 % Denna funktion returnerar ett lågpass filter som representerar ögat. I
 % detta fall betraktningsavståndet har satts till 500 mm och punkternas
 % storlek till 0.0847. Observera att punktens storlek motsvarar ett tryck
@@ -77,21 +91,22 @@ for i = 1:size(OnePlus6_LAB_N(:, 1, 1))
 end
 
 % Calculate mean and max values in difference
-LAB_mean_halftone_N = mean(mean(LAB_diff_1Plus6_SN))    %
-LAB_max_halftone_N = max(max(LAB_diff_1Plus6_SN))       %
+LAB_mean_OnePlus6_N = mean(mean(LAB_diff_1Plus6_SN))    % 0.0345
+LAB_max_OnePlus6_N = max(max(LAB_diff_1Plus6_SN))       % 83.6210
 
 %% Calculate SCIE Lab differense, Requires original
+% Create lightsource
 CIED65 = [95.05, 100, 108.9];
-%CIEA = ??;
+CIEA = [109.85, 100.00, 35.58];
 
 ppi = 120;
 d = 5; % inch
 sampPerDeg = ppi * d * tan(pi/180);
 
-OnePlus6_SCIELab = scielab(sampPerDeg, original_XYZ, OnePlus6_XYZ, CIED65, 'xyz');
+OnePlus6_SCIELab = scielab(sampPerDeg, original_XYZ, OnePlus6_XYZ, CIEA, 'xyz');
 
-SCIE_mean_nearest = mean(mean(OnePlus6_SCIELab))   %
-SCIE_max_nearest = max(max(OnePlus6_SCIELab))      %
+SCIE_mean_nearest = mean(mean(OnePlus6_SCIELab))   % 1.9047
+SCIE_max_nearest = max(max(OnePlus6_SCIELab))      % 24.1767
 
 %% Calculate graininess, Requires SCIELAB
 OnePlus6_SCIELab_1 = scielab(sampPerDeg, OnePlus6_XYZ);
@@ -100,11 +115,11 @@ OnePlus6_L1 = OnePlus6_SCIELab_1(:,:, 1);
 OnePlus6_a1 = OnePlus6_SCIELab_1(:,:, 2);
 OnePlus6_b1 = OnePlus6_SCIELab_1(:,:, 3);
 
-graniness_OnePlus6 = sum(std(OnePlus6_L1(:)) + std(OnePlus6_a1(:)) + std(OnePlus6_b1(:)))    % 
+graniness_OnePlus6 = sum(std(OnePlus6_L1(:)) + std(OnePlus6_a1(:)) + std(OnePlus6_b1(:)))    % 0.1562
 
 %% Calculate SSIM, Requires original
 
-[OnePlus6_SSIMVal, OnePlus6_SSIMMap] = ssim(OnePlus6_RGB, original_RGB);    %
+[OnePlus6_SSIMVal, OnePlus6_SSIMMap] = ssim(OnePlus6_RGB, original_RGB);    % 0.9561
 
 figure;
 imshow(OnePlus6_SSIMMap);
